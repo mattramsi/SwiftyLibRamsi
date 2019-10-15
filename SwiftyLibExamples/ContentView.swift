ba7701336575dca55f254d9ba0947d5ba4ef4556
Matheus <matheus.ramos@trigg.com.br>
2019-10-15 11:11:45 -0300

    Initial project setup

---
+++ SwiftyLibExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftyLibExamples
+//
+//  Created by Matheus Ramos on 15/10/19.
+//  Copyright © 2019 Matheus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftyLibRamsi/SwiftyLibRamsi.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLibRamsi.h
+//  SwiftyLibRamsi
+//
+//  Created by Matheus Ramos on 15/10/19.
+//  Copyright © 2019 Matheus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibRamsi.
+FOUNDATION_EXPORT double SwiftyLibRamsiVersionNumber;
+
+//! Project version string for SwiftyLibRamsi.
+FOUNDATION_EXPORT const unsigned char SwiftyLibRamsiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibRamsi/PublicHeader.h>
+
+
